32212b4937d6a9ed5a973452a896da5ce4f22c61
keneth9501 <Usuario@CONTADOR>
2022-01-31 13:08:23 -0600

    controlador agencia

--- Controllers/AgenciaController.cs
+++ Controllers/AgenciaController.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-
+using WebAppCA.Models;
 namespace WebAppCA.Controllers
 {
     public class AgenciaController : Controller
@@ -13,5 +12,102 @@ namespace WebAppCA.Controllers
         {
             return View();
         }
+        
+        public JsonResult Listar()
+        {
+            List<Agencia> lAgencia = new List<Agencia>();
+            using (Model db = new Model())
+            {
+                lAgencia = db.Agencia.ToList();
+                db.Configuration.LazyLoadingEnabled = false;
+            }
+            return Json(new { data = lAgencia }, JsonRequestBehavior.AllowGet);
+        }
+       
+
+
+
+        public JsonResult Obtener(int idagencia)
+        {
+            Agencia oAgencia = new Agencia();
+
+            using (Model db = new Model())
+            {
+
+                oAgencia = (from p in db.Agencia.Where(x => x.id_agencia == idagencia)
+                            select p).FirstOrDefault();
+                db.Configuration.LazyLoadingEnabled = false;
+            }
+
+            return Json(oAgencia, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Guardar(Agencia oAgencia)
+        {
+            
+            bool respuesta = true;
+            try
+            {
+                if (oAgencia.id_agencia == 0)
+                {
+                    using (Model db = new Model())
+                    {
+                        db.Agencia.Add(oAgencia);
+                        db.SaveChanges();
+                        db.Configuration.LazyLoadingEnabled = false;
+                    }
+                }
+                else
+                {
+                    using (Model db = new Model())
+                    {
+                        Agencia tempagencia = (from p in db.Agencia
+                                                 where p.id_agencia == oAgencia.id_agencia
+                                                 select p).FirstOrDefault();
+
+                        
+                        tempagencia.Nombre_agencia = oAgencia.Nombre_agencia;
+
+
+                        db.SaveChanges();
+                        db.Configuration.LazyLoadingEnabled = false;
+                    }
+                }
+            }
+            catch(Exception E)
+            {
+                Console.WriteLine(E.Message);
+                respuesta = false;   
+
+            }
+            
+            return Json(new { resultado = respuesta}, JsonRequestBehavior.AllowGet);
+            
+         
+        }
+
+        public JsonResult Eliminar(int idagencia)
+        {
+            bool respuesta = true;
+            try
+            {
+                using (Model db = new Model())
+                {
+                    Agencia oAgencia = new Agencia();
+                    oAgencia = (from p in db.Agencia.Where(x => x.id_agencia== idagencia)
+                                 select p).FirstOrDefault();
+                    db.Agencia.Remove(oAgencia);
+                    db.SaveChanges();
+                    db.Configuration.LazyLoadingEnabled = false;
+                }
+            }
+            catch
+            {
+                respuesta = false;
+            }
+            return Json(new { resultado = respuesta }, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
